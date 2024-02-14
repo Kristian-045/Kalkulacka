665bdc7c4a724bdefbed8c586db01868fcd7df0e
Kristian-045 <kaleta.kristian@gmail.com>
2024-02-14 10:52:54 +0100

    Project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+**Prostredi**
+------
+Windows 64bit
+
+**Autori**
+------
+
+Number cruncher
+- xdzurim00 Michal Dzurík
+- xkaletak00 Kristián Kaleta
+- xvlcekm00 Matej Vlček 
+
+**Licence**
+-------
+GNU v3
---
+++ src/main.js
@@ -0,0 +1,53 @@
+'use sctrict';
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+
+
+  mainWindow.loadURL(startUrl);
+  mainWindow.setTitle('Kalkulacka');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', function () {
+    createWindow();
+
+    if (process.env.NODE_ENV === 'development') {
+        // Development specific settings
+    }
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
+
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
