33b64c388c8fa121f6ababb147e5bfe8e08748fb
Mathas05 <mathas05@gmail.com>
2024-03-24 18:15:49 +0100

    Added a lot of tests and fixed some functions
    Still needs work, some tests need polishing

--- src/math.js
+++ src/math.js
@@ -1,30 +1,55 @@
 module.exports = {
+
     addition: function(x, y) {
-        return x + y;
+        return ( x + y);
     },
     subtraction: function(x, y) {
-        return x - y;
+        return ( x - y);
     },
     multiplication: function (x, y) {
-        return x * y;
+        return (x * y);
     },
     division: function (x, y) {
-        return x / y;
+        if (y === 0) {
+            throw new Error('Division by zero!');
+        }
+        return (x / y);
     },
     factorial: function (x) {
-        let sum = 0;
-        for (let i = x; i > 0; i--) {
-            sum += x * sum;
+        if (x === 0 || x === 1) {
+            return 1;
         }
-        return sum;
+        if (x < 0) {
+            throw new Error('Factorial of negative number!');
+        }
+        if ((x % 1) !== 0) {
+            throw new Error('Factorial of floating number!');
+        }
+        let sum = 1;
+        for (let i = x; i > 0; i--) {
+            sum = i * sum;
+        }
+        return (sum);
     },
     exponential: function (x, y) {
-        return x ** y;
+        return (x ** y);
     },
     rooting: function (x, y) {
-        return x ** (1/y);
+        if (y === 0) {
+            throw new Error('Cannot compute 0th root!');
+        }
+        if (x < 0) {
+            throw new Error('Cannot compute negative number')
+        }
+        if (y % 2 === 0) {
+            throw new Error('Factorial of floating number!');
+        }
+        return (x ** (1/y));
     },
     modulo: function (x, y) {
-        return x % y;
-    }
+        if (y === 0) {
+            throw new Error('Division by zero!');
+        }
+        return (x % y);
+    },
 }
--- src/tests.js
+++ src/tests.js
@@ -3,6 +3,97 @@ const math = require('./math');
 
 describe('Testing adding numbers function', function (){
     it('should add number right',function(){
-        assert.equal(math.add(1,2),3);
+        assert.equal(math.addition(1,2),3);
+        assert.equal(math.addition(-2,2),0);
+        assert.equal(math.addition(-5,-4),-9);
+        assert.equal(math.addition(0,0),0);
+        assert.equal(math.addition(0.5656,2.26496),2.83056);
     })
 });
+
+describe('Testing subtracting numbers function', function (){
+    it('should subtract number right',function(){
+        assert.equal(math.subtraction(1,2),-1);
+        assert.equal(math.subtraction(-2,2),-4);
+        assert.equal(math.subtraction(-5,-4),-1);
+        assert.equal(math.subtraction(0,0),0);
+        assert.equal(math.subtraction(0.5656,2.26496),-1.69936);
+    })
+});
+
+describe('Testing subtracting numbers function', function (){
+    it('should multiply number right',function(){
+        assert.equal(math.multiplication(1,2),2);
+        assert.equal(math.multiplication(-2,2),-4);
+        assert.equal(math.multiplication(-5,-4),20);
+        assert.equal(math.multiplication(0,0),0);
+        assert.equal(math.multiplication(0.5656,2.26496),1.281061376);
+    })
+});
+
+describe('Testing subtracting numbers function', function (){
+    it('should divide number right',function(){
+        assert.equal(math.division(1,2),0.5);
+        assert.equal(math.division(-2,2),-1);
+        assert.equal(math.division(-20,-4),5);
+        assert.equal(math.division(0.5656,2.26496),0.2497174343034756);
+        assert.equal(math.division(1,3),0.3333333333333333);
+    })
+    it('should not divide number right',function(){
+        assert.equal(math.division(10,0),Error);
+    })
+});
+
+describe('Testing factorial of numbers function', function (){
+    it('should do factorial right',function(){
+        assert.equal(math.factorial(1),1);
+        assert.equal(math.factorial(5),120);
+        assert.equal(math.factorial(15),1307674368000);
+        assert.equal(math.factorial(0),1);
+    })
+    it('should not do factorial right',function(){
+        assert.throws(math.factorial(-2));
+        assert.throws(math.factorial(0.5656));
+    })
+});
+
+describe('Testing exponential numbers function', function (){
+    it('should return the right number',function(){
+        assert.equal(math.exponential(1,5),1);
+        assert.equal(math.exponential(-2,2),4);
+        assert.equal(math.exponential(-5,-4),-0.0016);
+        assert.equal(math.exponential(25,0.5),5);
+        assert.equal(math.exponential(0.5,3),0.125);
+        assert.equal(math.exponential(7,1),7);
+        assert.equal(math.exponential(42,0),1);
+    })
+});
+
+describe('Testing rooting of numbers function', function (){
+    it('should root number right',function(){
+        assert.equal(math.rooting(1,2),1);
+        assert.equal(math.rooting(1,3),1);
+        assert.equal(math.rooting(0.5656,4),0.8672161363);
+    })
+    it('should not root number right',function(){
+        assert.equal(math.rooting(-125,2),Error);
+        assert.equal(math.rooting(8,2.26496),Error);
+        assert.equal(math.rooting(8,0),Error);
+        assert.equal(math.rooting(-125,3),-5);
+    })
+});
+
+describe('Testing modulo of numbers function', function (){
+    it('should return modulo right',function(){
+        assert.equal(math.modulo(1,2),1);
+        assert.equal(math.modulo(-2,2),0);
+        assert.equal(math.modulo(-20,-4),0);
+        assert.equal(math.modulo(0.5656,2.26496),0.5656);
+        assert.equal(math.modulo(77,3),2);
+    })
+    it('should not return modulo right',function(){
+        assert.equal(math.modulo(10,0),Error);
+    })
+});
+
+
