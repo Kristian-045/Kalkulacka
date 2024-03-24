af31a83f2ab3f2926698911c1e8153690bcabdd8
Mathas05 <mathas05@gmail.com>
2024-03-24 15:43:50 +0100

    Adding math functions
    Functions added:
    addition, subtraction, multiplication, division, factorial, exponential, rooting, rooting

--- src/math.js
+++ src/math.js
@@ -1,5 +1,30 @@
 module.exports = {
-    add: function (x, y) { 
-        return x + y; 
-    }, 
+    addition: function(x, y) {
+        return x + y;
+    },
+    subtraction: function(x, y) {
+        return x - y;
+    },
+    multiplication: function (x, y) {
+        return x * y;
+    },
+    division: function (x, y) {
+        return x / y;
+    },
+    factorial: function (x) {
+        let sum = 0;
+        for (let i = x; i > 0; i--) {
+            sum += x * sum;
+        }
+        return sum;
+    },
+    exponential: function (x, y) {
+        return x ** y;
+    },
+    rooting: function (x, y) {
+        return x ** (1/y);
+    },
+    modulo: function (x, y) {
+        return x % y;
+    }
 }
