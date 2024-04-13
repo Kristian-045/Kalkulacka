b84404a8ce833dde86513988117411b4fd2dd119
Atopes <roman.andrascik177@gmail.com>
2024-04-13 18:57:39 +0200

    Added parse function
    Added function "parse" that uses supporting functions "findSpecialChar" and "calc", the function calculates the problem in string and returns the result of the problem or error message.

--- src/script.js
+++ src/script.js
@@ -2,10 +2,15 @@
 const calculatorDisplay = document.getElementById('calculatorDisplay');
 const calculatorButtons = document.querySelectorAll('.calculator-button');
 
+// Importing math module 
+const math = require('./math');
+
 // Regex for only characters we support
 const regexAll = /^[0-9+\/*s!-]+$/;
 // Regex for operators
 const regexOperators = /^[+\/*s!-]+$/;
+// Special characters sorted by PEMDAS weight
+const regexOpWeighted = ["^", "√","!","%","*","/","+","-"]; 
 
 
 // button click support
@@ -105,4 +110,107 @@ function inputNumber(number){
 
 function backspace(){
     calculatorDisplay.value = calculatorDisplay.value.substring(0,calculatorDisplay.value.length - 1)
+}
+ let errBool, errMsg;  
+// Works on PEMDAS basis
+// Returns the result of the problem
+function parse(probString){
+    let direction=1,rightIndex,leftIndex,subStringReplaced,subStringCalculated;
+    errBool=0;
+    //get rid of all parenthesis
+    for(let i = 0; i < probString.length; i+=direction){
+        if(probString[i] === ")" && direction === 1){
+            rightIndex = i;
+            direction = -1;
+        }else if(probString[i] === "(" && direction === -1){
+            leftIndex = i;
+            subStringReplaced = probString.substring(leftIndex,rightIndex);
+            subStringCalculated = probString.substring(leftIndex+1,rightIndex-1);
+            // replace the expresssion in () with the result and change dir back to 1
+            probString.replace(subStringReplaced,findSpecialChar(subStringCalculated));
+            if(errBool===1){
+                return errMsg;
+            }
+            direction = 1;
+        }
+    }
+    // calculate the rest of the problem without the parenthesis
+    return findSpecialChar(probString);
+}
+function findSpecialChar(stringToCalc){
+    let result=stringToCalc,leftSide,rightSide,RIndex,LIndex;
+    for(let i = 0; i < regexOpWeighted.length; i++){ // goes through the special characters that represent mathematical operations
+        for(let j = 0; j < stringToCalc.length; j++){ // goes through the string
+            if(stringToCalc[j]===regexOpWeighted[i]){
+                //find leftsideIndex and create leftSide substring
+                for(let k=j-1; k > 0;k--){
+                    if(isNaN(stringToCalc[k]) && stringToCalc[k] !== "." && stringToCalc[k] !== " "){
+                        LIndex = k;
+                        leftSide = stringToCalc.subString(k,j-1);
+                        break;
+                    }
+                }
+                //find rightsideIndex and creare rightSide substring
+                for(let k=j+1; k < stringToCalc.length ; k++){
+                    if(isNaN(stringToCalc[k]) && stringToCalc[k] !== "." && stringToCalc[k] !== " "){
+                        RIndex = k;
+                        rightSide = stringToCalc.subString(k,j+1);
+                        break;
+                    }
+                }
+                // create substring that is to be replaced by the result
+                let replacethis = stringToCalc.subString(LIndex,RIndex);
+                // replace the substring in result by number calculated from calc
+                result.replace(replacethis,calc(leftSide,rightSide,regexOpWeighted[i]))
+                if(errBool===1){
+                    return errMsg;
+                }
+            }
+        }
+    }
+    return result;
+}
+// returns the result of expression
+function calc(leftSide,rightSide,specialChar){
+    if(specialChar == '^'){
+        return math.exponential(leftSide,rightSide);
+    }else if(specialChar == '√'){
+        if(isNaN(math.rooting(rightSide,leftSide))){
+            errBool = 1;
+            errMsg = math.rooting(rightSide,leftSide);
+            return "";
+        }else{
+            return math.rooting(rightSide,leftSide);
+        }
+    }else if(specialChar == '!'){
+        if(isNaN(math.factorial(leftSide))){
+            errBool = 1;
+            errMsg = math.factorial(leftSide);
+            return "";
+        }else{
+            return math.factorial(leftSide);
+        }
+    }else if(specialChar == '%'){
+        if(isNaN(math.modulo(leftSide,rightSide))){
+            errBool = 1;
+            errMsg = math.modulo(leftSide,rightSide);
+            return "";
+        }else{
+            return math.modulo(leftSide,rightSide);
+        }
+    }else if(specialChar == '*'){
+        return math.multiplication(leftSide,rightSide);
+    }else if(specialChar == '/'){
+        if(isNaN( math.division(leftSide,rightSide))){
+            errBool = 1;
+            errMsg = math.division(leftSide,rightSide);
+            return "";
+        }else{
+            return math.division(leftSide,rightSide);
+        }
+    }else if(specialChar == '+'){
+        return math.addition(leftSide,rightSide);
+    }else if(specialChar == '-'){
+        return math.subtraction(leftSide,rightSide);
+    }
 }
