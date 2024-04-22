49eb633e90f0823e36c472182628433f0e587906
Michal-Dzurík <misko7104@gmail.com>
2024-04-22 10:05:32 +0200

    Adding installation and uninstallation scripts

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,1 +1,15 @@
-sudo rm -r /*
+#!/bin/bash
+
+# Define variables
+APP_NAME="butcalculator"
+
+# Ask for confirmation before uninstalling
+read -p "Are you sure you want to uninstall $APP_NAME? (y/n): " choice
+if [ "$choice" != "y" ]; then
+  echo "Uninstallation canceled."
+  exit 0
+fi
+
+sudo dpkg -r "$APP_NAME"
+
+echo "Uninstallation completed."
